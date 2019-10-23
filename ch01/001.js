a0cd1a2d22460bbddebaeb741548fbe44956af58
lucifer <azl397985856@gmail.com>
2019-10-23 17:57:27 +0800

    Update 001.js

--- ch01/001.js
+++ ch01/001.js
@@ -1,1 +1,16 @@
-function test() {}
+function LSS(list) {
+  const len = list.length;
+  let max = -Number.MAX_VALUE;
+  let sum = 0;
+  for (let i = 0; i < len; i++) {
+    sum = 0;
+    for (let j = i; j < len; j++) {
+      sum += list[j];
+      if (sum > max) {
+        max = sum;
+      }
+    }
+  }
+
+  return max;
+}
